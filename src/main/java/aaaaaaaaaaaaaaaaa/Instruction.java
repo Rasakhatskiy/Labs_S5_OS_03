805b1104acdd82c25f1a542b83d3e26037fafd4d
Rasakhatskyi <saharok.maks@gmail.com>
2021-12-11 14:27:09 +0200

    copied source from MOSS

---
+++ src/main/java/aaaaaaaaaaaaaaaaa/Instruction.java
@@ -0,0 +1,12 @@
+package aaaaaaaaaaaaaaaaa;
+
+public class Instruction {
+    public String inst;
+    public long addr;
+
+    public Instruction(String inst, long addr) {
+        this.inst = inst;
+        this.addr = addr;
+    }
+
+}
